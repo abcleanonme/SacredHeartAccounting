54fc24aefde15c40e7b89fe06c384c7cceeba54e
abcleanonme <dillonangle@gmail.com>
2014-02-26 08:47:06 -0600

    comments

---
+++ SacredHeartAccounting/SacredHeartAccounting.cpp
@@ -0,0 +1,19 @@
+/*	Dillon Angle Case Martin Khadijah McDonald E.G.
+	MM.DD.2014	Spring 2014
+	CSC2144		E Period
+
+	GROUP PROJECT
+
+	In keeping with the Honor Code of the Department of
+	Computer Science, I affirm that I have neither given
+	nor received assistance on this assignment.
+
+						Signature____________________________
+
+						Signature____________________________
+
+						Signature____________________________
+
+						Signature____________________________
+						*/
+
